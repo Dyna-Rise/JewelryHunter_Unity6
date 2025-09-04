64ce3d799daa0a69fa0472af343b808ceae8b814
DESKTOP-C5T1MTL\user <dynarise.headquarters@gmail.com>
2025-09-04 14:31:19 +0900

    カメラの限界値プログラムを追加

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,12 @@ public class CameraController : MonoBehaviour
     GameObject player;
     float x, y, z; //カメラの座標を決めるための変数
 
+    [Header("カメラの限界値")]
+    public float leftLimit;
+    public float rightLimit;
+    public float bottomLimit;
+    public float topLimit;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,7 +27,25 @@ public class CameraController : MonoBehaviour
         x = player.transform.position.x;
         y = player.transform.position.y;
 
+        if (x < leftLimit)
+        {
+            x = leftLimit;
+        }
+        else if (x > rightLimit)
+        {
+            x = rightLimit;
+        }
+
+        if (y < bottomLimit)
+        {
+            y = bottomLimit;
+        }
+        else if (y > topLimit)
+        {
+            y = topLimit;
+        }
+
         //取り決めた各変数x,y,zの値をカメラのポジションとする
-        transform.position = new Vector3(x,y,z);
+        transform.position = new Vector3(x, y, z);
     }
 }
